d7f8e2d0aefcd94a459b594f35c632adea466086
leeyoujun <muenzz119@naver.com>
2021-11-01 20:09:35 +0900

    chore: 주석달았음

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -12,12 +12,17 @@ router.get("/check_email", authRegisterAPI.MemberCheckEmail);
 
 //인증 이메일 보내기 API
 router.post("/send_auth_email", authSendEmailAPI.SendAuthEmail);
+
+//패스워드 변경 이메일 전송 API
 router.post(
   "/send_change_password_email",
   authSendEmailAPI.SendChangePasswordEmail
 );
+
+//인증키 확인
 router.post("/check_authkey", authRegisterAPI.MemberCheckAuthKey);
 
+//JWT 확인
 router.use("/check", jwt_check);
 router.get("/check", authRegisterAPI.TokenCheck);
 module.exports = router;
